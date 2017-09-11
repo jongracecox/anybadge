976e2cda7998a32c5ac86a0c24c277532e45df60
Jon Grace-Cox <jon.grace-cox@dunnhumby.com>
2017-09-11 17:20:34 -0400

    Added setup.py for package build.

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import sys
+import re
+from setuptools import setup
+
+def get_version():
+    with open("anybadge.py", "r") as file_handle:
+        file_text = file_handle.read()
+    version_match = re.search(r'.* __version__ = [\'"]([^\'"]*)[\'"]', file_text)
+    if version_match:
+        print version_match.group(1)
+        return version_match.group(1)
+    raise RuntimeError("Unable to identify version string.")
+
+setup(
+    name='anybadge',
+    description='Simple, flexible badge generator for project badges.',
+    version=get_version(),
+    author='Jon Grace-Cox',
+    author_email='jongracecox@gmail.com',
+    py_modules = ['anybadge'],
+    setup_requires=['setuptools', 'wheel'],
+    tests_require=[],
+    install_requires=[],
+    data_files=[],
+    options={
+        'bdist_wheel': {'universal': True}
+    },
+    platform=['any'],
+    url='https://github.com/jongracecox/anybadge'
+)
