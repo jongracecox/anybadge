6465c451c4af18cbb732f0e051dcb9734febdb5d
Jon Grace-Cox <30441316+jongracecox@users.noreply.github.com>
2019-07-11 21:17:28 -0400

    Fix widths (#24)
    * Update badge widths
    
    Switch to using get_text_width(' ') to get padding width.
    This results in a more compact badge as the font_width that was used before
    was the guesstimated font width that was too large.
    
    * Update default num padding chars
    
    This commit updates the number of padding characters to zero.  Many issues have been
    raised around producing more compact badges, so it feels like the most common usage would be
    with this option defaulted to zero.

--- anybadge.py
+++ anybadge.py
@@ -20,7 +20,7 @@ __uri__ = "https://github.com/jongracecox/anybadge"
 # Set some defaults
 DEFAULT_FONT = 'DejaVu Sans,Verdana,Geneva,sans-serif'
 DEFAULT_FONT_SIZE = 11
-NUM_PADDING_CHARS = 0.5
+NUM_PADDING_CHARS = 0
 DEFAULT_COLOR = '#4c1'
 DEFAULT_TEXT_COLOR = '#fff'
 MASK_ID_PREFIX = 'anybadge_'
@@ -94,7 +94,7 @@ class Badge(object):
         value(str): Badge value text.
         font_name(str, optional): Name of font to use.
         font_size(int, optional): Font size.
-        num_padding_chars(float): Number of padding characters to use to give extra
+        num_padding_chars(float, optional): Number of padding characters to use to give extra
             space around text.
         template(str, optional): String containing the SVG template.  This should be valid SVG
             file content with place holders for variables to be populated during rendering.
@@ -395,9 +395,10 @@ class Badge(object):
 
             >>> badge = Badge('pylint', '5')
             >>> badge.badge_width
-            103
+            65
         """
-        padding = int(self.font_width * (self.num_padding_chars + 3))
+        padding_char_width = self.get_text_width(' ')
+        padding = int(padding_char_width * (self.num_padding_chars + 3))
         return padding + self.label_width + self.value_width
 
     @property
